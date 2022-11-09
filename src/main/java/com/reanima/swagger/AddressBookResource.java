ab24d97a221be09ac2d7c4887b236eac911894b5
r34n1m4 <vadimzuravskis@gmail.com>
2022-11-09 21:43:32 +0200

    Swagger-UI added

--- src/main/java/com/reanima/swagger/AddressBookResource.java
+++ src/main/java/com/reanima/swagger/AddressBookResource.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 @RestController
+@RequestMapping("/api")
 public class AddressBookResource {
 
     ConcurrentMap<String, Contact> contacts = new ConcurrentHashMap<>();
--- src/main/java/com/reanima/swagger/SwaggerApplication.java
+++ src/main/java/com/reanima/swagger/SwaggerApplication.java
@@ -2,7 +2,9 @@ package com.reanima.swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class SwaggerApplication {
 
